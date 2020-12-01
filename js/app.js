2ade80f1339b45919eb1d7a33232ac65bd94073f
MaratAbramson <MaratAbramson@gmail.com>
2020-12-01 18:25:53 +0400

    add demo visual select

--- js/app.js
+++ js/app.js
@@ -1,43 +1,50 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        gender: NaN,
-        location: NaN,
-        questionId: false,
-        questionText: false,
-        final: false,
-        hide: false
-    },
+  el: '#app',
+  data: {
+    gender: NaN,
+    location: NaN,
+    questionId: false,
+    questionText: false,
+    final: false,
+    hide: false,
+    figureLocation: '',
+  },
 
-    methods: {
-        rerun(){
-            document.location.href = "/";
-        },
-        start(){
-          app.hide = true;
-          app.send(false);
-        },
-        send (choise) {
-            const answer = {
-                gender: app.gender,
-                location: app.location,
-                questionId: app.questionId,
-                choise: choise
-        }
-            axios.post('/', answer, {
-                headers:{
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json'
-                }
-            })
-                .then((response) => {
-                    app.questionId = response.data.questionId;
-                    app.final = response.data.final;
-                    app.questionText = response.data.questionText;
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-        },
-    }
+  methods: {
+    rerun() {
+      document.location.href = "/";
+    },
+    start() {
+      app.hide = true;
+      app.send(false);
+    },
+    send(choise) {
+      const answer = {
+        gender: app.gender,
+        location: app.location,
+        questionId: app.questionId,
+        choise: choise
+      }
+      axios.post('/', answer, {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+          }
+        })
+        .then((response) => {
+          app.questionId = response.data.questionId;
+          app.final = response.data.final;
+          app.questionText = response.data.questionText;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    },
+    setLocation() {
+      app.isActive = true;
+      app.figureLocation = event.target.parentNode.id;
+      app.location = event.target.parentNode.getAttribute('data-location-value');
+  }
+
+}
 })
